b667908ad15abc2b9a5c3e52426aaade5b681066
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-21 14:31:00 +0530

    [ajay]-Valided First Name

---
+++ User_Registration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace User_Registration
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            user_Contact firstn= new user_Contact();
+            firstn.Firstname("Ajay");
+            firstn.Firstname("ajay");
+
+        }
+    }
+}
---
+++ User_Registration/user_Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace User_Registration
+{
+    internal class user_Contact
+    {
+        public string Firstname(string name) 
+        {
+            string firstnameregex = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regexfirst = new Regex(firstnameregex);
+            if (regexfirst.IsMatch(name))
+            {
+                Console.WriteLine("Name is valid");
+                return name;
+            }
+            else
+            {
+                Console.WriteLine("Name is invalid");
+                return null;
+            }
+        }
+    }
+}
